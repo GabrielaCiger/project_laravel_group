42906ee2a2872c6dfc2bfaad84b6dedd805b5d73
Gabriela <gabriela.cigerova@le-campus-numerique.fr>
2024-07-29 16:09:55 +0200

    I changed the routage and homepage

---
+++ project/resources/views/homepage.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Homepage</title>
+        <link rel="stylesheet" href="../css/app.css">
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+
+    <h1>HOMEPAGE</h1>
+    <p>This is going to be a very pretty page.</p>
+
+    <a href="/product">See the list of products</a><br>
+    <a href="/cart">Go to cart</a><br>
+
+    </body>
+</html>
